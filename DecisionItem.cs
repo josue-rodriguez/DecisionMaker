0de1a23499f7f4a9c0c7680ee833715e3ee1c6a8
josue-rodriguez <josue.rodriguez594@gmail.com>
2020-04-21 23:55:34 -0700

    Add CSS styling for add/remove buttons

---
+++ DecisionItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DecisionMaker
+{
+    public class DecisionItem
+    {
+        public string Title { get; set; }
+        public bool IsBoring { get; set; }
+
+    }
+}
