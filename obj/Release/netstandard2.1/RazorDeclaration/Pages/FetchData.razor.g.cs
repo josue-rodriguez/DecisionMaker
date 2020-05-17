34a0d4e8061c5fb01c49f77b8d1534f57bcb9c19
josue-rodriguez <josue.rodriguez594@gmail.com>
2020-05-16 18:55:24 -0700

    fix typos

--- obj/Release/netstandard2.1/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ obj/Release/netstandard2.1/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -69,6 +69,13 @@ using DecisionMaker.Shared;
 #line default
 #line hidden
 #nullable disable
+#nullable restore
+#line 9 "C:\Users\josue\GitHub\DecisionMaker\_Imports.razor"
+using Blazored.LocalStorage;
+
+#line default
+#line hidden
+#nullable disable
     [Microsoft.AspNetCore.Components.RouteAttribute("/fetchdata")]
     public partial class FetchData : Microsoft.AspNetCore.Components.ComponentBase
     {
