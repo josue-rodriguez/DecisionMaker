0e871633bda73c520ab91b433e42c0b827e49c9e
josue-rodriguez <josue.rodriguez594@gmail.com>
2020-05-22 15:38:37 -0700

    add television component

--- Activity.cs
+++ Activity.cs
@@ -7,61 +7,19 @@ using System.Threading.Tasks;
 
 namespace DecisionMaker
 {
-    public class Activity
-    {
-        public string Type { get; set; }
-        public List<string> Choices = new List<string>();
-        public string ChosenActivity;
-        static readonly Random random = new Random();
-
-        // Add Choices
-        public void AddChoice(string NewChoice)
-        {
-            bool isNewMovie = !Choices.Any(choice => choice == NewChoice);
-            bool isNotEmpty = !string.IsNullOrWhiteSpace(NewChoice);
-            if (isNewMovie & isNotEmpty)
-            {
-                Choices.Add(NewChoice);
-            }
-        }
-
-
-        // Remove Choices
-        public void RemoveChoice(string ChoiceToRemove)
-        {
-            var itemToRemove = Choices.Single(choice => choice == ChoiceToRemove);
-            Choices.Remove(itemToRemove);
-        }
-
-        // Make a Choice
-        public void MakeChoice()
-        {
-            bool isNotEmpty = Choices.Any();
-            
-            if (isNotEmpty)
-            {
-                int index = random.Next(Choices.Count);
-                ChosenActivity = Choices[index];
-            } else
-            {
-                ChosenActivity = string.Empty;
-            }
-        }
-    }
-
-    public class Choice2
+    public class Choice
     {
         public string Name { get; set; }
     }
 
 
-    public class Activity2
+    public class Activity
     {
         static readonly Random random = new Random();
 
         public string Decision;
 
-        public List<Choice2> Items { get; set; } = new List<Choice2>();
+        public List<Choice> Items { get; set; } = new List<Choice>();
 
         //Make a decision
         public void MakeDecision()
